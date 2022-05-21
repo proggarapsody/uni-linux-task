a8fb221d1976d8b961aeb9b2c83a052555e683a3
Aleksey <aleksey@aleksey-klm.com>
2022-05-21 14:28:09 +0300

    feat: edit script.sh

--- script.sh
+++ script.sh
@@ -9,21 +9,21 @@ apt-get update
 apt-get install 1 dialog 
 
 # Dialog - install wget packege?
-dialog --title "Delete file" \
+dialog --title "Wget install" \
 --backtitle "Web App installer" \
---yesno "Hello firstly you must download wget package" 7 60
+--yesno "Hello! firstly you must download wget package" 7 60
 response=$?
 case $response in
-   0) echo "Continue";;
-   1) echo "Goodbye!"; exit;;
-   255) echo "Goodbye!"; exit;;
+   0) echo; clear; "Continue";;
+   1) echo; clear;"Goodbye!"; exit;;
+   255) echo; clear; "Goodbye!"; exit;;
 esac
 
 apt install wget
 wget -O site.zip https://github.com/proggarapsody/uni-linux-task/archive/main.zip
 unzip -o site.zip -x "*.git/*"
 rm site.zip
-cd uni-linux-task
+cd uni-linux-task-main
 
 # Working with images and videos
 cd media
@@ -36,7 +36,10 @@ video.sh # download video, convert their and delete video-in folder
 # Working with server
 apt install apache2
 cd ..
-mv . /var/www
+rm -rf /var/www/html/*
+mv -f ./* /var/www/html
+
+systemctl start apache2 
 
 # Working with firefox
 firefox --new-window localhost
