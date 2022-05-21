ee9d1e026fcfc4ed79bb9fb5630775b1d23dce3f
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-05-21 14:58:33 +0300

    feat: edit scripts

--- media/image-convert.sh
+++ media/image-convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt install imagemagick
+
 cd photo-in
 
 name=1
@@ -7,7 +9,7 @@ name=1
 for image in *.*;
 do
 convert "$image" -quality 30 ../photo-out/$name.jpg
-echo $name
+echo "$image"
 let name=name+1
 done
 
