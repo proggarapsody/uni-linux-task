79925625d18b5838755e4f6e22ad63b4b0c5f7c6
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-05-22 13:57:42 +0300

    feat: edit video source

--- script.sh
+++ script.sh
@@ -19,10 +19,10 @@ sudo apt install unzip
 sudo wget -O site.zip https://github.com/proggarapsody/uni-linux-task/archive/main.zip
 sudo unzip -o site.zip -x "*.git/*"
 sudo rm site.zip
-sudo cd uni-linux-task-main
+cd uni-linux-task-main
 
 # Working with images and videos
-sudo cd media
+cd media
 # Dialog - run image-convert script?
 sudo bash image-convert.sh # convert image and delete photo-in folder
 # Dialog - run video script?
@@ -33,10 +33,10 @@ sudo rm video.sh
 # Dialog - install apache2 package?
 # Working with server
 sudo apt install apache2
-sudo cd ..
+cd ..
 sudo rm -rf /var/www/html/*
 sudo mv -f ./* /var/www/html
-sudo cd .. 
+cd .. 
 sudo rm -rf uni-linux-task-main
 
 sudo systemctl start apache2 
