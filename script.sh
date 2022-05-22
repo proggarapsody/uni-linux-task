9d17a470bcf5a9b8b07791aa1d152a95c9c74f66
Aleksey <aleksey@aleksey-klm.com>
2022-05-22 12:03:56 +0300

    fix: edit commands

--- script.sh
+++ script.sh
@@ -23,9 +23,9 @@ cd uni-linux-task-main
 # Working with images and videos
 cd media
 # Dialog - run image-convert script?
-image-convert.sh # convert image and delete photo-in folder
+bash image-convert.sh # convert image and delete photo-in folder
 # Dialog - run video script?
-video.sh # download video, convert their and delete video-in folder
+bash video.sh # download video, convert their and delete video-in folder
 
 # Dialog - install apache2 package?
 # Working with server
@@ -34,7 +34,7 @@ cd ..
 rm -rf /var/www/html/*
 mv -f ./* /var/www/html
 cd .. 
-rm uni-linux-task-master
+rm -rf uni-linux-task-main
 
 systemctl start apache2 
 
