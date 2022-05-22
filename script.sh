c0412d3a4c74e4fa52f5480a60a5114d913228c3
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-05-22 13:56:25 +0300

    feat: edit video source

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt-get update 
-apt-get install 1 dialog 
+sudo apt-get update 
+sudo apt-get install 1 dialog 
 
 # Dialog - install wget packege?
-dialog --title "Wget install" \
+sudo dialog --title "Wget install" \
 --backtitle "Web App installer" \
 --yesno "Hello! firstly you must download wget package" 7 60
 response=$?
@@ -14,33 +14,33 @@ case $response in
    255) echo; clear; "Goodbye!"; exit;;
 esac
 
-apt install wget
-apt install unzip
-wget -O site.zip https://github.com/proggarapsody/uni-linux-task/archive/main.zip
-unzip -o site.zip -x "*.git/*"
-rm site.zip
-cd uni-linux-task-main
+sudo apt install wget
+sudo apt install unzip
+sudo wget -O site.zip https://github.com/proggarapsody/uni-linux-task/archive/main.zip
+sudo unzip -o site.zip -x "*.git/*"
+sudo rm site.zip
+sudo cd uni-linux-task-main
 
 # Working with images and videos
-cd media
+sudo cd media
 # Dialog - run image-convert script?
-bash image-convert.sh # convert image and delete photo-in folder
+sudo bash image-convert.sh # convert image and delete photo-in folder
 # Dialog - run video script?
-bash video.sh # download video, convert their and delete video-in folder
-rm image-convert.sh
-rm video.sh
+sudo bash video.sh # download video, convert their and delete video-in folder
+sudo rm image-convert.sh
+sudo rm video.sh
 
 # Dialog - install apache2 package?
 # Working with server
-apt install apache2
-cd ..
-rm -rf /var/www/html/*
-mv -f ./* /var/www/html
-cd .. 
-rm -rf uni-linux-task-main
+sudo apt install apache2
+sudo cd ..
+sudo rm -rf /var/www/html/*
+sudo mv -f ./* /var/www/html
+sudo cd .. 
+sudo rm -rf uni-linux-task-main
 
-systemctl start apache2 
+sudo systemctl start apache2 
 
 # Working with firefox
 
-sudo -k; firefox --new-window localhost
+firefox --new-window localhost
